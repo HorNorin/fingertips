74189efc662193f20693b8e7c19e9d57755240c4
Hor Norin <norin.hor@gmail.com>
2016-02-01 21:18:41 +0700

    Add restriction and refactor code

--- backend/app/controllers/api/v1/base_controller.rb
+++ backend/app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,21 @@
-class Api::V1::BaseController < ActionController::Base
-  include ApplicationHelper
+module Api
+  module V1
+    class BaseController < ::ActionController::Base
+      include ApplicationHelper
+
+      protected
+
+      def authenticate
+        unless user_logged_in?
+          render json: {message: 'Access denied.'}, status: 401
+        end
+      end
+
+      def none_authenticate
+        if user_logged_in?
+          render json: {message: 'You have already logged in.'}, status: 301
+        end
+      end
+    end
+  end
 end
--- backend/app/controllers/api/v1/session_controller.rb
+++ backend/app/controllers/api/v1/session_controller.rb
@@ -1,47 +1,43 @@
-class Api::V1::SessionController < Api::V1::BaseController
-  before_action :has_user_logged_in?, only: :create
+module Api
+  module V1
+    class SessionController < BaseController
+      before_action :none_authenticate, only: :create
 
-  def create
-    respond_to do |format|
-      format.json do
-        if user = User.authenticate(params[:email], params[:password])
-          user.update_attributes(token_expired_at: 1.day.from_now)
-          render json: {message: 'Successfully login.', data: user.to_json}
-        else
-          render json: {message: 'Invalid email or password.'}, status: 401
+      def create
+        respond_to do |format|
+          format.json do
+            if user = ::User.authenticate(params[:email], params[:password])
+              user.update_attributes(token_expired_at: 1.day.from_now)
+              render json: {message: 'Successfully login.', data: user.to_json}
+            else
+              render json: {message: 'Invalid email or password.'}, status: 401
+            end
+          end
         end
       end
-    end
-  end
 
-  def destroy
-    respond_to do |format|
-      format.json do
-        if user_logged_in?
-          access_token = current_user.access_token
+      def destroy
+        respond_to do |format|
+          format.json do
+            if user_logged_in?
+              access_token = current_user.access_token
 
-          begin
-            access_token = SecureRandom.uuid.gsub(/\-/, '')
-          end while User.exists?(access_token: access_token)
+              begin
+                access_token = ::SecureRandom.uuid.gsub(/\-/, '')
+              end while ::User.exists?(access_token: access_token)
 
-          current_user.update_attributes({
-            access_token: access_token,
-            token_expired_at: Time.zone.now
-          })
+              current_user.update_attributes({
+                access_token: access_token,
+                token_expired_at: Time.zone.now
+              })
 
-          render json: {message: 'Successfully logout.'}
-        else
-          render json: {message: 'You are not logged in.'}, status: 401
+              render json: {message: 'Successfully logout.'}
+            else
+              render json: {message: 'You are not logged in.'}, status: 401
+            end
+          end
         end
       end
-    end
-  end
-
-  private
-
-  def has_user_logged_in?
-    if user_logged_in?
-      render json: {message: 'You have already logged in.'}, status: 301
     end
   end
 end
