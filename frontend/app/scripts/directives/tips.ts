3c628f3d2999807a04026876cfd53e29b68a60cb
Hor Norin <norin.hor@gmail.com>
2016-02-04 21:13:15 +0700

    Refactor TipsDirective

--- frontend/app/scripts/directives/tips.ts
+++ frontend/app/scripts/directives/tips.ts
@@ -1,6 +1,69 @@
-import {Component, Input} from 'angular2/core';
-import {TipDirective} from './tip';
+import {Component, Input, AfterContentInit} from 'angular2/core';
 
+/*
+ * Private Component
+ * TipDirective
+ * @Input() value: any
+ * Render indivitual Tip
+ */
+@Component({
+  selector: 'tip',
+  template: `
+    <span class="tip-name">{{ title }}</span>
+    <span class="tip-duration">{{ duration }}</span>
+    <img class="tip-thumbnail" [attr.src]="thumbnail">
+    <a href="#" class="tip-play">&#8227;</a>
+  `
+})
+class TipDirective implements AfterContentInit {
+  title: string;
+  duration: string;
+  thumbnail: string;
+
+  @Input() value: any;
+
+  ngAfterContentInit() {
+    // Thumbnail string
+    this.thumbnail = this.value.thumbnail;
+
+    // Format duration string
+    var duration = this.value.duration;
+    var hours = Math.floor(duration / 3600);
+    var minutes = Math.floor((duration - (hours * 3600)) / 60);
+    var seconds = duration - (minutes * 60) - (hours * 3600);
+
+    if (hours > 0) {
+      this.duration = `${hours}:${this._format(minutes, 2)}:${this._format(seconds, 2)}`;
+    } else if (minutes > 0) {
+      this.duration = `${minutes}:${this._format(seconds, 2)}`;
+    } else {
+      this.duration = `${this._format(minutes, 2)}:${this._format(seconds, 2)}`;
+    }
+
+    // Format title string
+    this.title = this._shrinkName(this.value.title, 50);
+  }
+
+  private _format(num: number, length: number) {
+    var tmp: string = num.toString();
+    return tmp.length >= length ? num : new Array(length - tmp.length + 1).join('0') + tmp;
+  }
+
+  private _shrinkName(name: string, length: number) {
+    if (name.length > length) {
+      return `${name.substr(0, length - 3)}...`;
+    } else {
+      return name;
+    }
+  }
+}
+
+/*
+ * Public Component
+ * TipsDirective
+ * @Input() value: any[]
+ * Render a list of Tips
+ */
 @Component({
   selector: 'tips',
   template: `
@@ -9,7 +72,6 @@ import {TipDirective} from './tip';
     </div>
   `,
   directives: [TipDirective]
-  // styles: [require('../../styles/directives/tips.css')]
 })
 export class TipsDirective {
   @Input() value: any[];
